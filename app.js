3653d96be5ed7c626c54088b848f26ad2f80b2f4
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-03-20 20:21:51 -0700

    fixed indentation

--- app.js
+++ app.js
@@ -20,17 +20,17 @@ app.use(express.static('public'));
 app.use(express.static('source/views'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/src/views/index.html');
+    res.sendFile(__dirname + '/src/views/index.html');
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
-  });
+    console.log('a user connected');
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
 });
 
 http.listen(3000, function () {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
