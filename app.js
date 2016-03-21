254b7051b891ceaeeb4607464aca672b378af133
Li Xuanji <xuanji@quizlet.com>
2016-03-20 19:55:34 -0700

    basic socket.io

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
