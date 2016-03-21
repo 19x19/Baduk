258fe577423db9c998c5601fe08a771eb9b403d5
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-03-20 19:57:39 -0700

    no more CSP, added nosniff header

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var express = require('express');
 var app = express();
 
 // Global controller. Basically being used as middleware.
-// Currently used to add HTTP headers to all responses.
 app.get('/*', function(req, res, next) {
-    // HTTP headers to protect against general clickjacking
+    // General headers for security, ranging from clickjacking protection to
+    // anti-crawler protection. Note that the XSS protection only applies to
+    // IE8+ and Chrome, so still sanitize all input.
     res.header('X-Frame-Options', 'DENY');
-    res.header('Content-Security-Policy', 'frame-ancestors: \'none\'');
-    // No crawlers on this website, thanks
     res.header('X-Robots-Tag', 'noindex');
-    // Anti-XSS protection (only works on Chrome and IE8+, so sanitize anyways)
     res.header('X-XSS-Protection', '1; mode=block');
+    res.header('X-Content-Type-Options', 'nosniff');
 
     next();
 });
