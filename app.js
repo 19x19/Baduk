05da6e43fdba2d7bb03f3bf7e698e17b0b55a506
hitanshup <hitanshugold@gmail.com>
2016-03-20 23:09:53 -0400

    structuring 1.0

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('source/views'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -8,3 +11,4 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
