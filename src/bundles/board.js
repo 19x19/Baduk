15999134498d1c697b0b5e3d8eb75805a4f9cb8b
hitanshup <hitanshugold@gmail.com>
2016-03-26 11:36:21 -0400

    board improvement

--- src/bundles/board.js
+++ src/bundles/board.js
@@ -3,7 +3,6 @@ $(document).ready(function(e) {
     $('.board').click(function(e){
         var posX = (e.pageX - 48  - $(this).position().left);
         var    posY = (e.pageY - 70 - $(this).position().top);
-        //alert(posX + ' , ' + posY);
         var row = parseInt(posX / 57);
         var col = parseInt(posY / 57);
         if(row == 9) {
@@ -25,11 +24,14 @@ $(document).ready(function(e) {
 //REMOVES THE PIECE FROM THE BOARD
 remove = function(row, col) {
     var posToRemove = removePosition(row, col);
+    if(posToRemove == -1) { // The case where the element doesn't exist
+        return -1;
+    }
     arr = circles.split(","); // array of CSS background property of all the pieces on the board 
     arr.splice(posToRemove, 1); // removes the element from the array
     circles = '';
     for(var i = 0; i < arr.length; i++) {
-        if(!circles) {
+        if(circles) {
             circles = circles + ',' + arr[i].toString();
         } else {
             circles = arr[i].toString();
@@ -40,18 +42,13 @@ remove = function(row, col) {
 };
 
 
-// RETURNS WHICH PART OF THE STRING TO REMOVE (WHICH WOULD BASICALLY REMOVE THAT PIECE)
+// RETURNS WHICH PART OF THE STRING TO REMOVE
 removePosition = function(row, col) {
     arr = circles.split(","); // array of CSS background property of all the pieces on the board
-    //console.log("length: " + arr.length);
-    //console.log("arr: " + arr);
     var arr_2;
     for(var i = 0; i < arr.length; i++) {
-        //console.log(arr[i]);
         var test = arr[i].toString();
-        //console.log("test " + test);
         arr_2 = arr[i].toString().split(" ",3);
-        //console.log("arr2: " + arr_2); 
         if((arr_2[1].toString() == ((row * 55) + "px")) && 
             (arr_2[2].toString() == ((col * 55) + "px"))) {
                 return i;
@@ -66,11 +63,10 @@ add = function(row, col, type) {
     var posY = col * 55; 
     if(!circles) {
         circles = circles + 'url("../img/black_circle.png") ' + posX + 'px ' + posY + 'px no-repeat';
-        //alert(black_circles);
         $('.board').css("background", circles);
         $('.board').css("background-size", '60px');
     } else {
-        circles = circles + ', ' + 'url("../img/black_circle.png") ' + posX + 'px ' + posY + 'px no-repeat';
+        circles = circles + ',' + 'url("../img/black_circle.png") ' + posX + 'px ' + posY + 'px no-repeat';
         $('.board').css("background", circles);
         $('.board').css("background-size", '60px');
     }
