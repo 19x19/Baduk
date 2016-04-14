46b4146689c18b78f465569a1f8efc624de19ad2
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-14 01:18:11 -0700

    mocking black-hats

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 var config = {};
 config.port = 3001;
+
+// No Mr. Hacker, this secret key is not the same one as the production key. But
+// I really respect the efforts.
 config.session_key = "h0!9&nc7clz_6idaa!k0^9-gt4+!x9gi!o7_l_v-=fca9lh16c";
+
 module.exports = config;
