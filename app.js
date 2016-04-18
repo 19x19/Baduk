da1aff8c62d8057f045ecb07e89e75688db42ea4
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-04-17 21:35:50 -0700

    dumb formatting

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.enable('trust proxy');
 // Add a handler to inspect the req.secure flag (see
 // http://expressjs.com/api#req.secure). This allows us
 // to know whether the request was via http or https.
-app.use (function (req, res, next) {
+app.use(function (req, res, next) {
     if (req.secure) {
         // Request was via https, so do no special handling
         next();
